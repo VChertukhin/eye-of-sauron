c225564fd15c1b18c15e91c0a4ca0eb43d9291d0
VChertukhin <vchertukhin@protonmail.com>
2020-08-24 07:34:52 +0300

    basic metrics with visualisation

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import { SEND_AND_DROP_INTERVAL, VISUALISE_EVENTS } from './constants';
+import {
+    MetricsBuffer,
+    MouseMoveEvent,
+    MouseClickEvent,
+} from './interfaces/interfaces';
+import Observable from './observable';
+import { initMouseMoveHandler, initMouseClickHandler } from './events/events';
+import { sendMetricsBufferService } from './services/services';
+import { initEventsCanvas } from './utils/utils';
+
+const voidMetrics: MetricsBuffer = {
+    start: Date.now(),
+    mouseMoveBuffer: [],
+    mouseClickBuffer: [],
+};
+
+// metrics Buffer
+const metricsBuffer = new Observable<MetricsBuffer>(voidMetrics);
+// use event observables as atoms for metrics buffer and
+// at the same time as state with last event of atom type
+const mouseMoveAtom = new Observable<MouseMoveEvent | null>(null);
+const mouseClickAtom = new Observable<MouseClickEvent | null>(null);
+
+// prevent SSR break
+if (typeof window !== 'undefined') {
+    // create canvas to show all tracked events on page
+    if (VISUALISE_EVENTS) {
+        initEventsCanvas();
+    }
+
+    initMouseMoveHandler(mouseMoveAtom, metricsBuffer);
+    initMouseClickHandler(mouseClickAtom, metricsBuffer);
+
+    // periodicaly send metrics and drop atoms
+    setInterval(
+        () => {
+            // send metrics buffer to some service
+            sendMetricsBufferService(metricsBuffer.value);
+            // drop buffer
+            metricsBuffer.value = voidMetrics;
+        },
+        SEND_AND_DROP_INTERVAL,
+    );
+}
